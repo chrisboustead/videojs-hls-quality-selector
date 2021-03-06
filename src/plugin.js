e339afefc1ef38167101b972a41749fdaa791a2d
Ika Balzam <ika@codejitsu.ca>
2021-03-05 17:10:59 -0800

    Check if the level is enabled in the provided Quality Levels

--- src/plugin.js
+++ src/plugin.js
@@ -118,7 +118,10 @@ class HlsQualitySelectorPlugin {
           value: levels[i].height
         });
 
-        levelItems.push(levelItem);
+        // Check if the level is enabled
+        if (levels[i].enabled) {
+          levelItems.push(levelItem);
+        }
       }
     }
 
